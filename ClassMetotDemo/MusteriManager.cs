b88facb104967f7de48c2e17cf31c0fab523b6dc
ömer yıldırım <omer.1113@hotmail.com>
2021-01-18 23:24:54 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.FirstName} {musteri.LastName} bankaya eklenmiştir");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.FirstName} {musteri.LastName} bankadan silinmiştir");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine($"ADI: {musteri.FirstName}\nSOYADI: {musteri.LastName}\nYAŞ: {musteri.Age}\nDOĞUM YERİ: {musteri.City}\nTC NO: {musteri.AccountNumber}");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Musteri musteri = new Musteri
+            {
+                FirstName = "Ömer",
+                LastName = "Yıldırım",
+                Age = 25,
+                City = "Van",
+                AccountNumber = 12114546
+
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Add(musteri);
+            Console.WriteLine();
+            musteriManager.List(musteri);
+            Console.WriteLine();
+            musteriManager.Delete(musteri);
+
+        }
+    }
+}
